ffaca16979855c785926d5c0aae7e3ac154e558a
talalbismil <muhammadtalal057@gmail.com>
2026-07-25 17:23:26 +0500

    Add files via upload

---
+++ student.ts
@@ -0,0 +1,23 @@
+export const studentData = [
+  {
+    id: 1,
+    username: 'student',
+    password: '1234',
+
+    name: 'Talal',
+
+    attendance: 96,
+
+    marks: {
+      physics: 87,
+      chemistry: 91,
+      mathematics: 84,
+    },
+
+    homework: [
+      'Physics Chapter 4',
+      'Chemistry Worksheet',
+      'Mathematics Practice Set',
+    ],
+  },
+];
