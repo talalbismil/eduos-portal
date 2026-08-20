e1da404cfdbc86970ca43065324b81fb3211d4bd
talalbismil <muhammadtalal057@gmail.com>
2026-08-20 12:19:15 +0500

    Refactor Sidebar component to use functional syntax

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,31 +1,31 @@
 import Link from "next/link";
 
+export default function Sidebar() {
+  return (
+    <nav>
+      <Link href="/student">
+        Dashboard
+      </Link>
 
-<Link href="/student">
-Dashboard
-</Link>
+      <Link href="/student/subjects">
+        Subjects
+      </Link>
 
+      <Link href="/student/homework">
+        Homework
+      </Link>
 
-<Link href="/student/subjects">
-Subjects
-</Link>
+      <Link href="/student/marks">
+        Marks
+      </Link>
 
+      <Link href="/student/attendance">
+        Attendance
+      </Link>
 
-<Link href="/student/homework">
-Homework
-</Link>
-
-
-<Link href="/student/marks">
-Marks
-</Link>
-
-
-<Link href="/student/attendance">
-Attendance
-</Link>
-
-
-<Link href="/student/resources">
-Resources
-</Link>
+      <Link href="/student/resources">
+        Resources
+      </Link>
+    </nav>
+  );
+}
