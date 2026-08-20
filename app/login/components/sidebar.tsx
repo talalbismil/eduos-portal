0d9ef970bbd0a2795f0fa3f69e3d1d3fd1c08a26
talalbismil <muhammadtalal057@gmail.com>
2026-08-20 15:22:22 +0500

    Refactor Sidebar component with improved structure

--- app/login/components/sidebar.tsx
+++ app/login/components/sidebar.tsx
@@ -1,31 +1,51 @@
-import Link from "next/link";
+'use client';
 
+import Link from 'next/link';
 
-<Link href="/student">
-Dashboard
-</Link>
+export default function Sidebar() {
+  return (
+    <nav className="space-y-2">
+      <Link
+        href="/student"
+        className="block rounded-lg px-4 py-3 hover:bg-gray-100"
+      >
+        Dashboard
+      </Link>
 
+      <Link
+        href="/student/subjects"
+        className="block rounded-lg px-4 py-3 hover:bg-gray-100"
+      >
+        Subjects
+      </Link>
 
-<Link href="/student/subjects">
-Subjects
-</Link>
+      <Link
+        href="/student/homework"
+        className="block rounded-lg px-4 py-3 hover:bg-gray-100"
+      >
+        Homework
+      </Link>
 
+      <Link
+        href="/student/marks"
+        className="block rounded-lg px-4 py-3 hover:bg-gray-100"
+      >
+        Marks
+      </Link>
 
-<Link href="/student/homework">
-Homework
-</Link>
+      <Link
+        href="/student/attendance"
+        className="block rounded-lg px-4 py-3 hover:bg-gray-100"
+      >
+        Attendance
+      </Link>
 
-
-<Link href="/student/marks">
-Marks
-</Link>
-
-
-<Link href="/student/attendance">
-Attendance
-</Link>
-
-
-<Link href="/student/resources">
-Resources
-</Link>
+      <Link
+        href="/student/resources"
+        className="block rounded-lg px-4 py-3 hover:bg-gray-100"
+      >
+        Resources
+      </Link>
+    </nav>
+  );
+}
